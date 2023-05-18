be9a7e66ec5a446a6d847070b625cda6de9fd828
“anjana <anjanananayakkara1999@gmail.com>
2023-05-18 22:20:06 +0530

    hellow node

---
+++ NODEJS-TUTO/test/hello.js
@@ -0,0 +1,14 @@
+// ----------console write----------
+
+// console.log("hellow,world");
+// console.log("i love node js");
+
+
+//-------------variable---------
+
+// let firstName = "anuradha";
+
+// let a = 20;
+// let b = 30;
+
+// console.log(a+b);
